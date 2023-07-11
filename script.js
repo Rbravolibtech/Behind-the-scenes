5426ba44bc0fe9f753681d8f6389e1f7f6604d1b
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-11 16:35:21 -0600

    hoisting and tdz practice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Scoping in Practice
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
@@ -31,3 +31,54 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+*/
+
+/*========== Hoisting and TDZ IN PRACTICE ==========*/
+
+//Variables
+
+// console.log(me);
+// // //console.log(job);
+// // console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// //FUNCTIONS
+// console.log(addDecl(2, 3));
+// //console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// // EXAMPLE
+// console.log(undefined);
+
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products Deleted!');
+// }
+
+// var x = 1;
+
+// let y = 2;
+
+// const z = 4;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+/*========== THE THIS KEYWORD IN PRACTICE ==========*/
